487acbd6c638f48878111d2c36bc1c960a432245
kuznetsov_e <jackk@yandex.ru>
2017-06-17 22:01:20 +0300

    User login action fail was added.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import {Text} from 'react-native';
 import {emailChanged, passwordChanged, loginUser} from '../actions';
 import {Card, CardSection, Input, Button} from './common';
 
@@ -37,6 +38,9 @@ class LoginForm extends Component {
 
                     />
                 </CardSection>
+                <Text style={styles.errorTextStyle}>
+                    {this.props.error}
+                </Text>
                 <CardSection>
                     <Button onPress={this.onButtonPress}>Login</Button>
                 </CardSection>
@@ -45,10 +49,18 @@ class LoginForm extends Component {
     }
 }
 
-const mapStateToProps = state => {
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
+    }
+}
+
+const mapStateToProps = ({auth}) => {
+    const {email, password, error } = auth;
     return {
-        email: state.auth.email,
-        password: state.auth.password,
+        email, password, berror
     }
 
 };
