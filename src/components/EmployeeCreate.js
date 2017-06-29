ba26d4deae2ea63c82f1165cebb0c020c3857dc5
kuznetsov_e <jackk@yandex.ru>
2017-06-29 21:26:00 +0300

    Login finished.

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,5 +1,7 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 import {Card, CardSection, Input, Button} from './common';
+import {employeeUpdate} from "../actions/EmployeeActions";
 
 class EmployeeCreate extends Component {
     render() {
@@ -9,14 +11,16 @@ class EmployeeCreate extends Component {
                     <Input
                         label="Name"
                         placeholder="Jane"
-
+                        value={this.props.name}
+                        onChangeText={value => this.props.employeeUpdate({prop: 'name', value})}
                     />
                 </CardSection>
                 <CardSection>
                     <Input
                         label="Phone"
                         placeholder="555-5555-555"
-
+                        value={this.props.phone}
+                        onChangeText={value => this.props.employeeUpdate({prop: 'phone', value})}
                     />
                 </CardSection>
                 <CardSection>
@@ -34,4 +38,9 @@ class EmployeeCreate extends Component {
     }
 }
 
-export default EmployeeCreate;
+const mapStateToProps = (state) => {
+    const {name, phone, shift} = state.employeeForm;
+    return {name, phone, shift};
+}
+
+export default connect(mapStateToProps, {employeeUpdate})(EmployeeCreate);
